5119ccc7ab093dc736138e06b2cbbd8f84f161de
Valery <v.eliseev@fbs.com>
2019-08-02 19:49:34 +0300

    1)fix errors

--- HtmlUnit.pas
+++ HtmlUnit.pas
@@ -194,7 +194,7 @@ begin
       HTML  := TStringList.Create;
       HTML.WriteBOM := false;
       filename := wwwpath + Host +RequestPage;
-      HTML.LoadFromFile(filename, TEncoding.UTF8);
+      HTML.LoadFromFile(filename);
       AResponseInfo.ContentEncoding := 'utf-8';
       HTML.Text := Localization_HTML(RequestPage, HTML.Text);
       AResponseInfo.ContentType := GenContType(RequestPage);
